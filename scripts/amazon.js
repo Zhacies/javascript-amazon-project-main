d70ac4ad3919186510c7f872c34cf1350136565b
AbrahamGabalfin <abrahamgabalfin9a@gmail.com>
2025-09-29 14:26:08 +0800

    generate html with JavaScript

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,40 +1,37 @@
 const products = [{
-image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-rating: { 
-  starts: 4.5,
-  count: 87
-},
-priceCents:1090
-
+  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090
 },
 {
   image:'images/products/intermediate-composite-basketball.jpg',
   name:'Intermediate Size Basketball',
-  rating:{
-    starts:4,
-    count:127
+  rating: {
+    stars: 4,
+    count: 127
   },
-  priceCents: 2095,
-
+  priceCents: 2095
 },
 {
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-      stars:4.5,
-      count:56
-    },
-    priceCents:799
-
-  }
+  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name:'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799
+  },
 ];
 
 let productsHTML = '';
 
-products.forEach((product) => {
+products.forEach( (product) =>{
   productsHTML += `
-        <div class="product-container">
+      <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -53,7 +50,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${product.priceCents / 100}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -82,8 +79,9 @@ products.forEach((product) => {
             Add to Cart
           </button>
         </div>
-   `; 
-  }
-);
+  `;
+
+});
 console.log(productsHTML);
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector('.js-products-grid')
+  .innerHTML = productsHTML;
