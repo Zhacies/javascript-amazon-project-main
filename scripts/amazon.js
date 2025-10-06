b8e0b18f4903c5740f5c4674734b99c6ca749c35
AbrahamGabalfin <abrahamgabalfin9a@gmail.com>
2025-10-06 19:17:31 +0800

    dayjs library

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,9 @@ import {cart, addToCart} from '../data/cart.js'; // 'as' to rename cart
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 // use import * as nameVariable from '../data/cart.js' access all in that file
+
+
+
 let productsHTML = '';
 
 products.forEach( (product) =>{
