762c2fb5b9f9fea71686b9e6ca6ee542b3120a26
AbrahamGabalfin <abrahamgabalfin9a@gmail.com>
2025-10-16 01:06:38 +0800

    create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,8 @@
-export let cart= JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
+
+export function loadFromStorage(){
+cart= JSON.parse(localStorage.getItem('cart'));
 if (!cart){   
 cart =[{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -9,6 +13,7 @@ cart =[{
         quantity: 1,
         deliveryOptionId: '2'
       }];
+}
 }
 
 function saveToStorage(){
